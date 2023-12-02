f761e1618fe7e4c5ad00dee5b2f5efef8473416d
codebeatme <codebeatme@outlook.com>
2023-12-02 09:13:46 +0800

    更新

---
+++ src/zh-hant/import_system/print_prefix.py
@@ -0,0 +1,9 @@
+'''
+本節文章
+https://learnscript.net/zh-hant/python-reference/import-system/module-search-path/directories-in-module-search-path/ Python 模組搜尋路徑中的目錄有哪些
+'''
+
+# 顯示標準/擴充程式庫目錄
+import sys
+print(sys.prefix)
+print(sys.exec_prefix)
---
+++ src/zh/import_system/print_prefix.py
@@ -0,0 +1,9 @@
+'''
+本节文章
+https://learnscript.net/zh/python-reference/import-system/module-search-path/directories-in-module-search-path/ Python 模块搜索路径中的目录有哪些
+'''
+
+# 显示标准/扩展库目录
+import sys
+print(sys.prefix)
+print(sys.exec_prefix)
