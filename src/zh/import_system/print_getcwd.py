d7fdfa68055e31e682caf050fe830f1843e56384
codebeatme <codebeatme@outlook.com>
2023-12-05 19:11:57 +0800

    更新

--- src/zh/import_system/print_getcwd.py
+++ src/zh/import_system/print_getcwd.py
@@ -1,3 +1,8 @@
+'''
+本节文章
+https://learnscript.net/zh/python-reference/import-system/module-search-path/relative-paths-in-module-search-path/ 如何在 Python 模块搜索路径中使用相对路径
+'''
+
 # 显示当前工作目录
 import os
 print(os.getcwd())
