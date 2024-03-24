f088514d3161051fa5a2f52eca60be6d5f715751
codebeatme <codebeatme@outlook.com>
2024-03-25 06:53:22 +0800

    更新

--- src/zh-hant/modules/hello.py
+++
@@ -1,7 +0,0 @@
-'''
-本節文章
-https://learnscript.net/zh-hant/python-reference/modules/ 什麽是 Python 模組
-'''
-
-# 模組 hello
-print('這裏是模組 hello！')
--- src/zh/modules/hello.py
+++
@@ -1,7 +0,0 @@
-'''
-本节文章
-https://learnscript.net/zh/python-reference/modules/ 什么是 Python 模块
-'''
-
-# 模块 hello
-print('这里是模块 hello！')
